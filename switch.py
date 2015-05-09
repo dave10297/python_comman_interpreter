1a15e66d86fa56912b7431253354fd76e53a6158
David A. Burns <dave10297@hotmail.com>
2015-05-08 22:31:54 -0400

    first commit

---
+++ switch.py
@@ -0,0 +1,19 @@
+
+# Implementation of switches as close to the implementation as found in C++
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
+
+    def __iter__(self):
+        yield self.match
+        raise StopIteration
+    
+    def match(self, *args):
+        if self.fall or not args:
+            return True
+        elif self.value in args:
+            self.fall = True
+            return True
+        else:
+            return False
